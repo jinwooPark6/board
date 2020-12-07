a296d5b26cd6ff633fe6d02e72099dbfedc91970
jinwooPark6 <jinwoo13245@naver.com>
2020-12-07 15:06:29 +0900

    2020.12.07 15:00

--- src/main/java/com/example/demo/utils/Printer.java
+++ src/main/java/com/example/demo/utils/Printer.java
@@ -1,8 +1,14 @@
 package com.example.demo.utils;
 
+import java.util.function.Consumer;
+
 import org.springframework.stereotype.Service;
 
 @Service
-public class Printer{
+public class Printer {
+    public void accept(String t) {
+        Consumer<String> c = System.out::println;
+        c.accept((String) t);
+    }
 
 }
